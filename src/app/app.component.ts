e84da78d9866c0276d1d53d5d718df837a0fe1c5
Ammar Israr <ammarisrar28@gmail.com>
2023-08-05 10:17:57 +0500

    Tweaked application padding and responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ export class AppComponent {
 
   constructor(public contactService: ContactService) {}
 
-  title = 'ammarIsrar';
+  title = 'Ammar Israr';
 }
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectShowcaseComponent } from './project-showcase/project-showcase.component';
+import { ProjectTileComponent } from './project-tile/project-tile.component';
 
 
 @NgModule({
   declarations: [
-    ProjectShowcaseComponent
+    ProjectShowcaseComponent,
+    ProjectTileComponent
   ],
   imports: [
     CommonModule,
